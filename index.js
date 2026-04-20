7d916658065105bc46d657b5651ed94aa4f9f077
Cristobal Pizarro Schkolnik <cristobalpizarroschkolnik@MacBook-Air-de-Cristobal-3.local>
2026-04-19 22:06:16 -0400

    Fix dotenv in production

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 process.on('uncaughtException', err => console.error('UNCAUGHT:', err));
 process.on('unhandledRejection', err => console.error('UNHANDLED:', err));
 const express = require('express');
